3ab99f36501ec2584002ab378b4f3d862b88ba3b
DamirPavlik <damir.pavlik@gmail.com>
2025-05-03 10:41:47 +0200

    feat: idk a bunch of stuff

---
+++ resources/views/register.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+</head>
+<body>
+    <form method="POST" action="/register">
+        @csrf
+        <input name="name" placeholder="Name">
+        <input name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <input type="password" name="password_confirmation" placeholder="Confirm Password">
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
